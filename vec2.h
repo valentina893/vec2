1fde5f5450d0f34a4b8559433e0d0062460f5f4d
Valentina <zenit@Valentinas-MacBook-Air.local>
2025-09-07 07:55:47 -0500

    added div, scaling, magnitude, and dp

--- vec2.h
+++ vec2.h
@@ -1,5 +1,7 @@
 // vec2.h
 
+#include <math.h>
+
 // 2D vector class for math, physics, and miscallaneous applications
 typedef struct vec2 {
     float x, y;
@@ -15,4 +17,21 @@ vec2 vec2_sub(vec2 a, vec2 b) {
 
 vec2 vec2_mult(vec2 a, vec2 b) {
     return (vec2){a.x * b.x, a.y * b.y};
+}
+
+vec2 vec2_div(vec2 a, vec2 b) {
+    if (b.x == 0 || b.y == 0) return a;
+    return (vec2){a.x / b.x, a.y / b.y};
+}
+
+vec2 vec2_scalar(vec2 a, float s) {
+    return (vec2){a.x * s, a.y * s};
+}
+
+float vec2_magnitude(vec2 a) {
+    return sqrtf(pow(a.x, 2) + pow(a.y, 2));
+}
+
+float vec2_dp(vec2 a, vec2 b) {
+    return a.x * b.x + a.y * b.y;
 }
